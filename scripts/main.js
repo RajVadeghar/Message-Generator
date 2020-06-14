c129d62a3ec03c9bd002f9431df888f105accc28
RajVadeghar <55544757+RajVadeghar@users.noreply.github.com>
2020-06-14 09:26:51 +0530

    Update main.js
    Added some people and also reduced some of the functions

--- scripts/main.js
+++ scripts/main.js
@@ -2,21 +2,14 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
-  return array[random];
-}
-
 let strangerText = 'Hey NAME, You are AWESOME. Love ya...'
 let strangerMembers = ['xyz', 'nishanth', 'santhoshi']
 let friendsText = "Hey NAME, I love you. Hope, we'll be nicer to each other upto the end."
-let friendMembers = ['abhilash', 'vamshi', 'vaishnavi', 'meghana', 'bachi', 'kamakshi', 'ganesh', 'asra', 'bhagi']
+let friendMembers = ['bachi', 'kamakshi', 'vaishnavi', 'meghana', 'ganesh', 'asra', 'bhagi']
 let familyText = 'Hey NAME, I Love U :)'
 let familyMembers = ['mom','dad']
-let enemyText = 'Hey NAME, Go fuck YOURSELF!!!'
-let enemyMembers = ['buddy']
-let myText = 'Welcome PROFESSOR :)'
-let me = ['raj vadeghar']
+let specialText = 'Hey NAME, Go fuck YOURSELF!!!'
+let specialMembers = ['buddy', 'abhilash', 'vamshi', 'subhash']
 
 let storyText = "Hey buddy! You shouldv'e asked Raj Vadeghar to make a statement for you. Ask him and get into this. Hope, you are having a good day :)"
 
@@ -41,14 +34,14 @@ function result() {
       newStory = newStory.replace('NAME', name);
       story.textContent = newStory;
     }
-    else if(enemyMembers.includes(name.toLowerCase())) {
-      let newStory = enemyText;
+    else if(specialMembers.includes(name.toLowerCase())) {
+      let newStory = specialText;
       newStory = newStory.replace('NAME', name);
       story.textContent = newStory;
     }
-    else if(me.includes(name.toLowerCase())) {
-      let newStory = myText;
-      story.textContent = newStory;
+    else if(name.toLowerCase() === 'raj vadeghar') {
+      let myMessage = 'welcome PROFESSOR :)';
+      story.textContent = myMessage;
     }
     else {
       let newStory = storyText;
